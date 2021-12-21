06e3019ed93fce63d584c8fdc8e8b0ee5a93f3a3
Tripp Lucas <tripppdx@gmail.com>
2021-12-21 12:02:17 -0800

    adds notes component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Header from './components/header/Header.js';
 import Footer from './components/footer/Footer.js';
 import SignUp from './components/sign-up/Sign-Up.js';
 import Login from './components/login/Login.js';
+import Notes from './components/notes/Notes.js';
 import './App.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <Header/>
+    <div className="App">
+      <Header />
       <p>hello world</p>
-      <Login/>
-      <SignUp/>
-      <Footer/>
+      <Login />
+      <SignUp />
+      <Notes />
+      <Footer />
     </div>
   );
 }
