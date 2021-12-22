360c059e71c63a26cf3781523228c4128b018c14
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-12-22 14:21:21 -0800

    refactors login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,49 +1,115 @@
-// import React, { useContext, useState } from 'react';
-// import { When } from 'react-if';
+import * as React from 'react';
+import { useContext } from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
+import { Alert } from '@mui/material';
+import { When, Unless } from "react-if";
 
-// import { AuthContext } from '../../context/auth.js';
+import { AuthContext } from '../../context/auth.js';
 
-// function Login() {
-//   let auth = useContext(AuthContext);
+function Login() {
 
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
+  let auth = useContext(AuthContext);
 
-//   function handleChange (e) {
-//     let { name, value } = e.target;
+  const [values, setValues] = React.useState({
+    username: '',
+    password: '',
+    showPassword: false,
+  });
 
-//     if (name === 'username') setUsername(value);
-//     if (name === 'password') setPassword(value);
-//   }
+  const handleChange = (value) => (event) => {
+    setValues({ ...values, [value]: event.target.value });
+  };
 
-//   async function handleSubmit (e) {
-//     e.preventDefault();
-//     await auth.login(username, password);
-//   }
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
 
-//   return (
-//     <>
-//       <When condition={auth.isLoggedIn}>
-//         <button onClick={auth.logout}>Log Out</button>
-//       </When>
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-//       <When condition={!auth.isLoggedIn}>
-//         <form onSubmit={handleSubmit}>
-//           <input
-//             placeholder="username"
-//             name="username"
-//             onChange={handleChange}
-//           />
-//           <input
-//             placeholder="password"
-//             name="password"
-//             onChange={handleChange}
-//           />
-//           <button type="submit">Login</button>
-//         </form>
-//       </When>
-//     </>
-//   );
-// }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await auth.login(values.username, values.password);
+    setValues('');
+  }
 
-// export default Login;
+  return (
+    <>
+      <When condition={!auth.isLoggedIn}>
+        <FormControl>
+          <Box sx={{ display: 'flex', flexWrap: 'nowrap' }}>
+            <div>
+              <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <FormControl id="usernameLogin" sx={{ m: 1, width: '25ch' }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-username"
+                    type={values ? 'text' : 'username'}
+                    value={values.username}
+                    onChange={handleChange('username')}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <AccountCircle sx={{ color: 'action.active', mr: -0.5, my: 0 }} />
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+              </Box>
+              <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  onChange={handleChange('password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+            </div>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginRight: '15px' }}>
+            <div>
+              <Button sx={{ width: '100px', border: '1px solid green', borderRadius: '10px' }} variant="outlined" onClick={handleSubmit}>
+                Login
+              </Button>
+              <When condition={values.password}>
+                <Alert severity={'success'} />
+                <Unless condition={!values.password}>
+                  <Alert severity="error">ERROR!</Alert>
+                </Unless>
+              </When>
+            </div>
+          </Box>
+        </FormControl>
+      </When>
+    </>
+  );
+}
+
+export default Login;
