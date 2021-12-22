a8ed8c806a42c8971857f2591aa077871ac7b29e
Rupinder Hayer <ruphayer@gmail.com>
2021-12-22 13:02:57 -0800

    added themme context api feature, closes #59

--- src/context/theme.js
+++ src/context/theme.js
@@ -1,1 +1,26 @@
+import React, { useState } from 'react';
+
+// our theme singleton
+export const ThemeContext = React.createContext();
+
+function Theme ({children}) {
  
+  const[mode,setMode]=useState('light');
+     
+  
+  
+ const values= {
+      mode,
+      setMode,
+    }
+  
+
+    return (
+      <ThemeContext.Provider value={values}>
+        {children}
+      </ThemeContext.Provider>
+    )
+  
+}
+
+export default Theme;
