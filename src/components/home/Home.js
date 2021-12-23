38bbd2afb8e576b6785831a42fb56189ffa7d478
Tripp Lucas <tripppdx@gmail.com>
2021-12-22 17:53:37 -0800

    closes #73; moves logout button into header

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,5 @@
 import RubberDucky from '../rubber-ducky/RubberDucky';
 import BreakReminder from '../break-reminder/BreakReminder';
-import Logout from '../logout/Logout';
 import Notes from '../notes/Notes.js';
 import Food from '../food/Food.js';
 import './home.scss';
@@ -8,13 +7,8 @@ import './home.scss';
 // TODO: cookie login is not working
 function Home(props) {
   return (
-
     <div className="home">
-
-  
       <Notes />
-      <Logout showSignup={props.showSignup} setShowSignup={props.setShowSignup} />
-
       <BreakReminder />
       <Food />
       <RubberDucky />
