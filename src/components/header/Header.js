c7cbbe014db35070781c7458657746207306c992
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-12-23 11:08:32 -0800

    styles header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,18 +5,20 @@ import './header.scss';
 import { ThemeContext } from '../../context/theme';
 import { useContext } from 'react';
 import Logout from '../logout/Logout';
+import { When } from 'react-if';
+import { AuthContext } from '../../context/auth.js';
+import lightLogo from '../../assets/codebrLogoLight.png'
 
 function Header(props) {
   let theme = useContext(ThemeContext);
+  let auth = useContext(AuthContext);
 
   const headerStyle = {
     dark: {
-      backgroundColor: 'darkSlateGrey ',
-      color: 'pink',
+      color: '#99adb0',
     },
     light: {
-      backgroundColor: 'pink',
-      color: 'darkSlateGrey',
+      color: '#4B575D',
     },
     common: {
       transition: 'all 1s ease',
@@ -31,27 +33,35 @@ function Header(props) {
   return (
     <Box sx={{ flexGrow: 1 }} data-testid="header">
       <div style={themeStyle} className="header1" position="static" id="navBar">
-        <Switch
-          className="switch-theme"
-          onClick={() =>
-            theme.setMode(theme.mode === 'light' ? 'dark' : 'light')
-          }
-        ></Switch>
-        <Typography
-          className="title"
-          variant="h3"
-          component="div"
-          sx={{ flexGrow: 1 }}
-          id="title"
-        >
-          codeBreak
-        </Typography>
-        <div id="logoutButtonDiv" style={{ width: '10%', display: 'flex' }}>
-          <Logout
-            showSignup={props.showSignup}
-            setShowSignup={props.setShowSignup}
-          />
+        {theme.mode === 'light' ? 
+                  <img src={lightLogo} alt="logo" id="title"/>
+                  : <img src={lightLogo} alt="logo" id="title"/>}
+        <div id="login-switch-container">
+          <Typography
+            className="loginHome"
+            variant="h5"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            id="title"
+            onClick={() => props.setEnter(true)}
+          >
+            login
+          </Typography>
+          <Switch
+            className="switch-theme"
+            onClick={() =>
+              theme.setMode(theme.mode === 'light' ? 'dark' : 'light')
+            }
+          ></Switch>
         </div>
+        <When condition={auth.isLoggedIn}>
+          <div id="logoutButtonDiv" style={{ width: '10%', display: 'flex' }}>
+            <Logout
+              showSignup={props.showSignup}
+              setShowSignup={props.setShowSignup}
+            />
+          </div>
+        </When>
       </div>
     </Box>
   );
