64fe9199e2ad5d7ab6e9582a90bad324bb84649f
Rupinder Hayer <ruphayer@gmail.com>
2021-12-27 16:32:43 -0800

    refactor food.test.js, add URL from env

--- src/__tests__/Food.test.js
+++ src/__tests__/Food.test.js
@@ -2,13 +2,13 @@ import { rest } from "msw";
 import { setupServer } from 'msw/node';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import {act} from 'react-dom/test-utils'
+import {act} from 'react-dom/test-utils';
 
 import Food from '../components/food/Food';
 
 const server = setupServer(
   
-  rest.post('https://code-break-server.herokuapp.com/food', (req, res, ctx) => {
+  rest.post(`${process.env.REACT_APP_URL}/food`, (req, res, ctx) => {
  
     return res(
       ctx.json({
@@ -26,7 +26,7 @@ afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 describe('Testing the Food Component', () => {
-it('Should send response when called upon food api', async()=>{
+it('Should send response when called upon food api and render data in Food Component', async()=>{
 
   render(<Food/>)
   const button = screen.getByRole('button')
