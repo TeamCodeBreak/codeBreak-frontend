d247ffb84e2b58d5c51b1e9d4cd1f3f4161c5fb7
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-12-28 22:19:11 -0800

    changes and styles break reminder gif

--- src/components/break-modal/Modal.js
+++ src/components/break-modal/Modal.js
@@ -3,15 +3,15 @@ import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
-import danceGif from '../../img/danceGif.gif';
+import Button from '@mui/material/Button';
+import happySun from '../../img/sun50.gif';
+import './breakModal.scss';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -19,12 +19,11 @@ const style = {
 };
 
 function BreakReminderModal(props) {
-
   return (
     <div>
       <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
+        aria-labelledby='transition-modal-title'
+        aria-describedby='transition-modal-description'
         open={props.open}
         onClose={props.handleClose}
         closeAfterTransition
@@ -35,12 +34,12 @@ function BreakReminderModal(props) {
       >
         <Fade in={props.open}>
           <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Great job dev!<br></br><img src={danceGif} alt="dancing gif" />
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Time to take a break. Go for it!
-            </Typography>
+            <div id='closeModalButton'>
+              <Button onClick={props.handleClose} variant='contained'>
+                x
+              </Button>
+            </div>
+            <img src={happySun} alt='smiling sun with break message' />
           </Box>
         </Fade>
       </Modal>
